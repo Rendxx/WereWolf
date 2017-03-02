9a6e3a91c47541e9ddba6d5c2045be361b98768f
Rendxx <rendxx@gmail.com>
2017-03-02 00:33:55 -0600

    test webpack setting

--- .webpack_config/webpack.common.js
+++ .webpack_config/webpack.common.js
@@ -5,49 +5,69 @@ var extractTextPlugin = new ExtractTextPlugin("./[name].css");
 var root = path.resolve(__dirname);
 
 module.exports = {
-    plugins: [
-      extractTextPlugin
-    ],
-    entry: {
-        host : './src/host/Index',
-        client : './src/client/Index',
-        test : './src/test/Index'
-    }, 
-    module: {
-       loaders: [
+  plugins: [
+    extractTextPlugin
+  ],
+  entry: {
+    host: './src/host/Index',
+    client: './src/client/Index',
+    test: './src/test/Index'
+  },
+  module: {
+    use: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          loaders: [
             {
-              test: /\.js|jsx$/,
-              exclude: /node_modules/,
-              loader: 'babel'
-            },
-            {
-              test: /\.css$/,
-              exclude: /node_modules/,
-              loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-            },
-            {
-              test: /\.less$/,
-              exclude: /node_modules/,
-              loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
-            },
-            {
-              test: /\.(png|jpg)$/,
-              exclude: /node_modules/,
-              loader: 'url-loader'
-            },
-            {
-              test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-              exclude: /node_modules/,
-              loader : 'file-loader?name=../Font/[name].[ext]'
+              loader: 'css-loader'
             }
-        ]
+          ]
+        })
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          loaders: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'less-loader'
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(png|jpg)$/,
+        exclude: /node_modules/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        exclude: /node_modules/,
+        loader: 'file-loader?name=../Font/[name].[ext]'
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      CLIENT: root + '/../src/client',
+      HOST: root + '/../src/host',
+      GLOBAL: root + '/../src/global',
     },
-    resolve: {
-        alias: {
-            CLIENT: root+'/src/client',
-            HOST: root+'/src/host',
-            GLOBAL: root+'/src/global',
-        },
-        extensions: ['.js', '.json', '.less']
-    }
+    extensions: ['.js', '.json', '.less']
+  }
 };
